ff78b2977f1f0082363f1c8b596230e209a53fe1
Grant Crofton <grantac@hotmail.com>
2014-10-19 17:55:54 +0100

    Initial files

---
+++ Lisp.fsx
@@ -0,0 +1,26 @@
+﻿type Value = int
+
+type Exp =
+| Atom of Value
+| Sexp of Exp * Exp
+| Car of Exp * Exp
+| Cdr of Exp * Exp
+| Cons of Exp * Exp
+| Eq of Exp * Exp
+| IsAtom of Exp
+
+let rec execute = function
+| Atom x -> x.ToString()
+| Sexp(exp1,exp2) -> "(" + execute exp1 + " " + execute exp2 + ")"
+| Car(exp1,exp2) -> execute exp1
+| Cdr(exp1,exp2) -> execute exp2
+| Cons(exp1,exp2) -> execute <| Sexp(exp1,exp2)
+| Eq(exp1,exp2) -> if exp1 = exp2 then "T" else "NIL"
+| IsAtom(Atom(_)) -> "T"
+| IsAtom(_) -> "NIL"
+
+execute <| Sexp( Atom(5), Car( Cons( Atom(6), Atom(50) ), Atom(3)) )
+
+execute <| Sexp( Eq( Atom(6), Atom(6)), Eq( Atom(6), Atom(7)))
+
+execute <| Sexp( IsAtom(Atom(5)), IsAtom(Sexp(Atom(8), Atom(7))))
---
+++ Lisp2.fsx
@@ -0,0 +1,34 @@
+﻿type Value =
+| Number of int
+| Text of string
+| List of Value * Value
+| Bool of bool
+
+let rec print = function
+| Number(i) -> i.ToString()
+| Text(t) -> t
+| List(val1,val2) -> "(" + print val1 + " " + print val2 + ")"
+| Bool(b) -> if b then "T" else "NIL"
+
+type Exp =
+| Atom of Value
+| Sexp of Exp * Exp
+| Func of string * Exp
+
+let rec exec = function
+| Atom x -> x
+| Sexp(exp1,exp2) -> List(exec exp1, exec exp2)
+| Func(name, exp) -> 
+    match name, exp with
+    | "car", Sexp(exp1,exp2) -> exec exp1
+    | "cdr", Sexp(exp1,exp2) -> exec exp2
+    | "cons", Sexp(exp1,exp2) -> exec <| Sexp(exp1,exp2)
+    | "eq", Sexp(exp1,exp2) -> if exp1 = exp2 then Bool(true) else Bool(false)
+    | "atom", Atom(_) -> Bool(true)
+    | "atom", _ -> Bool(false)
+    | x, exp -> failwith ("Function '" + x + "' not recognised, or incorrect parameters used")
+
+let execute expression = 
+    printfn "%s" <| print(exec expression)
+
+execute <| Func("eq", Sexp(Atom(Number(3)), Atom(Number(3))))
